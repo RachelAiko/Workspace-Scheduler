bdd57cd642f4f5ddcfa133df6ec8afd37f9d82cc
GantInnes <gjinnes4844@eagle.fgcu.edu>
2022-01-23 16:40:02 -0500

    condensed home and login components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+//import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
@@ -12,7 +12,7 @@ import { AuthGuard } from './services/auth.guard';
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
+    //{ path: 'login', component: LoginComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent},
     { path: 'signup', component: SignupComponent },
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
