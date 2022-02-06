502f232c642917fc74aecdde5b894f45e824ac30
codingConquest <chase.brendan.k@gmail.com>
2022-02-06 10:36:24 -0500

    Updated Exception types to display detailed errors

--- API/Helpers/ExceptionCustomMiddlewareExtension.cs
+++ API/Helpers/ExceptionCustomMiddlewareExtension.cs
@@ -19,22 +19,62 @@ namespace API.Helpers
                 {
                     await _next(httpContext);
                 }
+                catch (ArgumentException argumentEx)
+                {
+                    Console.WriteLine("Argument Exception");
+                    await HandleExceptionAsync(httpContext, argumentEx);
+                }
+                catch (FormatException FormatEx)
+                {
+                    Console.WriteLine("Format Exception");
+                    await HandleExceptionAsync(httpContext, FormatEx);
+                }
+                catch (FirebaseAdmin.Auth.FirebaseAuthException FirebaseAuthEx)
+                {
+                    Console.WriteLine("Firebase Auth Error");
+                    await HandleExceptionAsync(httpContext, FirebaseAuthEx);
+                }
                 catch (Exception ex)
                 {
                     await HandleExceptionAsync(httpContext, ex);
                 }
+                
             }
             private async Task HandleExceptionAsync(HttpContext context, Exception exception)
             {
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var message = exception switch
+                {
+                    ArgumentException => "Argument Exception", FormatException => "Invalid Authentication Token", 
+                    FirebaseAdmin.Auth.FirebaseAuthException => "Expired Authentication Token", _ => "Internal Server Error"
+                };
+
                 await context.Response.WriteAsync(new Error()
                 {
                     Code = context.Response.StatusCode,
-                    Message = "Internal Server Error from the custom middleware."
+                    Message = message
                 }.ToString());
             }
     }
 
 
 }
+
+
+/*
+
+
+Console.WriteLine("---------********************************---------");
+Console.WriteLine(e.Source);
+Console.WriteLine("------------------");
+Console.WriteLine(e.StackTrace);
+Console.WriteLine("------------------");
+Console.WriteLine(e.Message);
+//Console.WriteLine(e.GetType);
+Console.WriteLine("---------*********************************---------");
+
+
+
+*/
